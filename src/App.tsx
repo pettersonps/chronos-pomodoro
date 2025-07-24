49894243ac2f506e05989698d4cb67c899e6c039
Petterson Santos <pettersonsantos2001@gmail.com>
2025-07-24 16:57:22 -0300

    adjust DefaultInput and create DefaultButton

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -33,11 +36,10 @@ export function App() {
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
           <div className='formRow'>
-            <p>Ciclos:</p>
-            <p>0 0 0 0 0 0 0</p>
+            <Cycles />
           </div>
           <div className='formRow'>
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} color='green' />
           </div>
         </form>
       </Container>
