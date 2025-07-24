3c24d2e1db57c1888fd9641e5e701a672e1fe6b0
Petterson Santos <pettersonsantos2001@gmail.com>
2025-07-23 21:27:37 -0300

    global and theme CSS | Heading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { Heading } from "./components/Heading";
+import { Heading } from './components/Heading';
 
-import "./styles/theme.css";
-import "./styles/global.css";
+import './styles/theme.css';
+import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading>
+        Olá mundo!
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere,
         suscipit autem eos aliquam quibusdam voluptatibus! Quasi temporibus
