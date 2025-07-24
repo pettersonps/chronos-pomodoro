96fab4889f3d0a89759ca1c7cf34312dec638260
Petterson Santos <pettersonsantos2001@gmail.com>
2025-07-24 11:59:12 -0300

    CountDown and DefaultInput components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -16,10 +17,29 @@ export function App() {
         <Menu />
       </Container>
       <Container>
-        <Heading>FORM</Heading>
+        <CountDown />
       </Container>
       <Container>
-        <Heading>FOOTER</Heading>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              id='input'
+              type='text'
+              labelText='task:'
+              placeholder='digite algo'
+            />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos:</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
