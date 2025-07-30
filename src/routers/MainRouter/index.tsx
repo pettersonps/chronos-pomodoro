f766dcc7f2734e0bf98e14915bc0aedec7348d0c
Petterson Santos <pettersonsantos2001@gmail.com>
2025-07-30 16:34:16 -0300

    css general adjust | history page structure and css

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
 import { AboutPomodoro } from '../../pages/AboutPomodoro';
 import { Home } from '../../pages/Home';
+import { History } from '../../pages/History';
 import { NotFound } from '../../pages/NotFound';
 import { useEffect } from 'react';
 
@@ -19,6 +20,7 @@ export function MainRouter() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/history/' element={<History />} />
         <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
