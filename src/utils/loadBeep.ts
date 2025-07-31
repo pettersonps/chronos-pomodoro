05dc811a423e74827bd8bd7cf931592c4ab77d4b
Petterson Santos <pettersonsantos2001@gmail.com>
2025-07-30 23:04:34 -0300

    switch beep sound

--- src/utils/loadBeep.ts
+++ src/utils/loadBeep.ts
@@ -1,7 +1,7 @@
-import gravitationalBeep from '../assets/audios/gravitational_beep.mp3';
+import beep from '../assets/audios/beep.mp3';
 
 export function loadBeep() {
-  const audio = new Audio(gravitationalBeep);
+  const audio = new Audio(beep);
   audio.load();
 
   return () => {
